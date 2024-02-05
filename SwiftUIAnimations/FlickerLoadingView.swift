bd25d5d32c1ce9e73e25251970a33b05cdb63953
Rahul Jamba <rahuljamba.exe@gmail.com>
2024-02-05 23:28:01 +0530

    Added FlickerLoadingView Animation View

---
+++ SwiftUIAnimations/FlickerLoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  FlickerLoadingView.swift
+//  SwiftUIAnimations
+//
+//  Created by Rahul Jamba on 05/02/24.
+//
+
+import SwiftUI
+
+struct FlickerLoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlickerLoadingView()
+}
