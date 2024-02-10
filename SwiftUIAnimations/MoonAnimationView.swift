0fe6055d881ea5cde41e8ec465b7d69da547fbb3
Rahul Jamba <rahuljamba.exe@gmail.com>
2024-02-10 16:47:42 +0530

    Added MoonAnimation View For iPad

---
+++ SwiftUIAnimations/MoonAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MoonAnimationView.swift
+//  SwiftUIAnimations
+//
+//  Created by Rahul Jamba on 10/02/24.
+//
+
+import SwiftUI
+
+struct MoonAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoonAnimationView()
+}
