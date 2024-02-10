5d33fa192594c7858cf81b4b5d3f7f479a8cf402
Rahul Jamba <rahuljamba.exe@gmail.com>
2024-02-10 17:21:32 +0530

    OlegView Bottom Tabbar Animation

---
+++ SwiftUIAnimations/OlegView.swift
@@ -0,0 +1,18 @@
+//
+//  OlegView.swift
+//  SwiftUIAnimations
+//
+//  Created by Rahul Jamba on 10/02/24.
+//
+
+import SwiftUI
+
+struct OlegView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OlegView()
+}
