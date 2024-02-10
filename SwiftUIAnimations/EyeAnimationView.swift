caa02d6606556c7e435ae6f2dfdf494144aaed4c
Rahul Jamba <rahuljamba.exe@gmail.com>
2024-02-10 17:02:33 +0530

    EyeAnimation View

---
+++ SwiftUIAnimations/EyeAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  EyeAnimationView.swift
+//  SwiftUIAnimations
+//
+//  Created by Rahul Jamba on 10/02/24.
+//
+
+import SwiftUI
+
+struct EyeAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EyeAnimationView()
+}
