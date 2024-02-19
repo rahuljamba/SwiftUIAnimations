7c4f53f7b6a19cacf08a800741b575ea84ad421b
Rahul Jamba <rahuljamba.exe@gmail.com>
2024-02-19 11:44:04 +0530

    Email Validation View

---
+++ SwiftUIAnimations/EmailValidationFormView.swift
@@ -0,0 +1,60 @@
+//
+//  EmailValidationFormView.swift
+//  SwiftUIAnimations
+//
+//  Created by Rahul Jamba on 19/02/24.
+//
+
+import SwiftUI
+
+struct EmailValidationFormView: View {
+    
+    @State private var isSelected:Bool = false
+    @State var enterEmailText = String()
+    
+    var body: some View {
+        ZStack{
+         
+            Color("BG").ignoresSafeArea()
+            
+            Text(enterEmailText)
+                .foregroundColor(.black)
+                .padding(.top, 100)
+                .padding()
+            
+            HStack{
+                
+                
+                TextField("", text: $enterEmailText, prompt: Text("Enter valid email").foregroundColor(.white))
+                    .padding()
+                    .foregroundColor(.white)
+                    .onChange(of: enterEmailText) { newValue in
+                        isSelected = newValue.isValidEmail()
+                    }
+                
+                Image(systemName: "arrowshape.turn.up.right.fill")
+                    .foregroundColor(.white)
+                    .offset(x: isSelected ? -20 : 20)
+                    .animation(.easeIn, value: isSelected)
+                
+            }
+            .frame(height: 50)
+            .background(Color("Text"))
+            .cornerRadius(10)
+            .padding()
+            
+        }
+    }
+}
+
+#Preview {
+    EmailValidationFormView()
+}
+
+extension String {
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: self)
+    }
+}
